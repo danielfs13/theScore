ffad4ede90804aef7602cd2d6cad8e050be9d989
Daniel Strano <88854271+danielfs13@users.noreply.github.com>
2021-11-07 16:49:27 -0300

    Add files via upload
    Pequenas correções e estudos na seção do Campeonato Brasileiro.

---
+++ brasileiro.js
@@ -0,0 +1,87 @@
+var elapsedTime = document.querySelector("#elapsed");
+var homeTeamImage = document.querySelector("#homeLogo");
+var homeTeamName = document.querySelector("#homeName");
+var awayTeamImage = document.querySelector("#awayLogo");
+var awayTeamName = document.querySelector("#awayName");
+var lastMatchGoal = document.querySelector("#goals");
+var matchTable = document.querySelector("#matchTable");
+var eventsPlace = document.querySelector('#eventos');
+var eventsP = document.querySelector('#eventosP')
+
+
+function addMatchTile(data){
+    console.log(data)
+ 
+    var matchtile = document.createElement('div');
+    matchtile.classList.add("match-tile");
+
+  
+    var homeTeam = document.createElement('div');
+    homeTeam.classList.add("team");
+    
+
+    var homeTileTeamName = document.createElement('p');
+    homeTileTeamName.innerHTML = data['teams']['home']['name'];
+    var homeTileTeamLogo = document.createElement('img');
+    homeTileTeamLogo.src=data['teams']['home']['logo'];
+    homeTeam.appendChild(homeTileTeamLogo);
+    homeTeam.appendChild(homeTileTeamName);
+
+
+
+
+    var awayTeam = document.createElement('div');
+    awayTeam.classList.add("team");
+
+    var awayTileTeamName = document.createElement('p');
+    awayTileTeamName.innerHTML = data['teams']['away']['name'];
+    var awayTileTeamLogo = document.createElement('img');
+    awayTileTeamLogo.src=data['teams']['away']['logo'];
+    awayTeam.appendChild(awayTileTeamLogo);
+    awayTeam.appendChild(awayTileTeamName);
+
+
+    var score = document.createElement('p');
+    score.innerHTML = "x";
+
+    matchtile.appendChild(homeTeam);
+    matchtile.appendChild(score);
+    matchtile.appendChild(awayTeam);
+
+    matchTable.appendChild(matchtile);
+}
+
+
+fetch("https://v3.football.api-sports.io/fixtures?league=71&season=2021&next=10", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "v3.football.api-sports.io",
+		"x-rapidapi-key": "b5fd9ace06fe64728c81b99adf061751"
+	}
+})
+.then(response => response.json().then(data => {
+    var matchesList = data['response'];
+    var fixture = matchesList[0]['fixture'];
+    var goals = matchesList[0]['goals'];
+    var teams = matchesList[0]['teams'];
+
+    
+
+
+   elapsedTime.innerHTML = " ";
+   homeTeamImage.src = teams['home']['logo'];
+   homeTeamName.innerHTML = teams['home']['name'];
+   awayTeamImage.src = teams['away']['logo'];
+   awayTeamName.innerHTML = teams['away']['name'];
+   lastMatchGoal.innerHTML = "X";
+
+
+   for(var i = 1; i<matchesList.length;i++){
+       addMatchTile(matchesList[i]);
+   }
+
+
+}))
+.catch(err => {
+    console.log(err);
+});
